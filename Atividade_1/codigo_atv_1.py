a3aaa767858b7d3bfc064eb0f9726269f3eaa893
yasmozatti <yasmimmozatti@gmail.com>
2025-05-20 00:03:45 -0300

    Revisão final - bugs tratados

--- Atividade_1/codigo_atv_1.py
+++ Atividade_1/codigo_atv_1.py
@@ -1,67 +1,82 @@
 # APLICACAO OUVIDORIA
 # A ouvidoria seguirá um modelo onde o seu índice será o id
+# As manifestações são guardadas numa lista 'ouvidoria'
 ouvidoria = []
+
+# Pegamos o nome do usuário, apesar de não ter uso nas opções do menu do sistema
+print("BEM VINDO À OUVIDORIA!")
+print("PARA INICIAR SEU COMENTÁRIO, INFORME:")
 
 red = '\033[1;31m'
 yellow = '\033[1;33m'
 cyan = '\033[1;36m'
+green = '\033[1;32m'
 clean = '\033[0;0m'
+magenta = '\033[1;35m'
+nome = input("NOME: ").upper()
 
 while True:
-    # INICAREMOS COM A VISUALIZACAO DO MENU
-    print("\n")
     print("-" * 100)
     print(" " * 47 + "OPÇÕES" + " " * 47)
     print("-" * 100)
     print(
-        f'{red}[1]{clean} Listar manifestações'
-        f'\n{cyan}[2]{clean} Adicionar manifestão'
-        f'\n{red}[3]{clean} Procurar manifestação pelo indice'
-        f'\n{cyan}[4]{clean}Remover manifestação pelo indice'
-        f'\n{yellow}[5]{clean}Exibir quantidade de manifestações'
-        f'\n{yellow}[6]{clean}Sair e finalizar menu').upper()
-
-    pergunta = input('ESCOLHA: ')
-    resposta = int(pergunta)
+        f'{red}[1]{clean} LISTAR MANIFESTAÇÕES'
+        f'\n{cyan}[2]{clean} ADICIONAR MANIFESTAÇÃO'
+        f'\n{red}[3]{clean} EXIBIR QUANTIDADE DE MANIFESTAÇÕES'
+        f'\n{magenta}[4]{clean} PROCURAR MANIFESTAÇÃO PELO CÓDIGO'
+        f'\n{yellow}[5]{clean} REMOVER MANIFESTAÇÃO PELO CÓDIGO'
+        f'\n{green}[6]{clean} FINALIZAR E SAIR DO MENU')
+    pergunta = (input(f"{nome} DIGITE SUA ESCOLHA: "))
+    print("-" * 100)
+    
+    if pergunta.isdigit():
+        resposta = int(pergunta)
+    else:
+        print(f'{red}ENTRADA INVÁLIDA. É PERMITIDO APENAS SELECIONAR ALGUMA OPÇÃO DO MENU!')
+        print(f'EXECUTE O PROGRAMA {red} NOVAMENTE. {clean}')
+        break
 
     if resposta == 1:
         if len(ouvidoria) == 0:
-            print("NÃO HÁ MANIFESTAÇÕES FEITAS")
+            print(f"{red}NÃO HÁ MANIFESTAÇÕES FEITAS{clean}")
         else:
-            print("MANIFESTAÇÕES FEITAS:")
+            print(f"{red}MANIFESTAÇÕES FEITAS: {clean}")
             for i in range(len(ouvidoria)):
                 print((i+1) , ' - ' , ouvidoria[i])
     elif resposta == 2:
-        texto_ouvidoria = input("ESCREVA SUA MANIFESTAÇÃO: ").upper()
+        texto_ouvidoria = input(f"{nome}, ESCREVA SUA MANIFESTAÇÃO: ").upper()
         ouvidoria.append(texto_ouvidoria)
-        print("MANIFESTAÇÃO ADICIONADA COM SUCESSO")
+        print(f"{green}MANIFESTAÇÃO ADICIONADA COM SUCESSO{clean}")
     elif resposta == 3:
         quantidade = 0
         for i in ouvidoria:
             quantidade +=1
-        print('QUANTIDADE DE MANIFESTAÇÕES: ' , quantidade)
+        if quantidade == 0:
+            print(f'{red}NÃO HÁ MANIFESTAÇÕES FEITAS{clean}!!!!!')
+        else:
+            print(f'QUANTIDADE DE MANIFESTAÇÕES: {magenta}{len(ouvidoria)}{clean}')
     elif resposta == 4:
-        while True:
-            codigo_str = input('DIGITE O CÓDIGO (DÍGITO) DA MANIFESTAÇÃO: ')
-            if codigo_str.isdigit():
-                codigo = int(codigo_str)
-                if 1 <= codigo <= len(ouvidoria):
-                    print(ouvidoria[codigo - 1])
-                    break
-                else: 
-                    print('CÓDIGO INVÁLIDO, TENTE NOVAMENTE')
-            else:
-                print("ENTRADA INVÁLIDA, TENTE NOVAMENTE!")
+        codigo_str = input('DIGITE O CÓDIGO (DÍGITO) DA MANIFESTAÇÃO: ')
+        if codigo_str.isdigit():
+            codigo = int(codigo_str)
+            if 1 <= codigo <= len(ouvidoria):
+                print(ouvidoria[codigo - 1])
+            elif len(ouvidoria) == 0:
+                print(f'{red}CÓDIGO INVÁLIDO! NÃO HÁ MANIFESTAÇÕES FEITAS. {clean}')
+            else: 
+                print(f'{red}CÓDIGO INVÁLIDO, TENTE NOVAMENTE{clean}')
+        else:
+            print(f"{red}ENTRADA INVÁLIDA, TENTE NOVAMENTE!{clean}")
     elif resposta == 5:
         eliminar = int(input("INSIRA O ÍNDICE DA MANIFESTAÇÃO QUE DESEJA ELIMINAR: "))
         if eliminar > len(ouvidoria) or eliminar <= 0:
-            print('INDICE INVÁLIDO, TENTE NOVAMENTE!')
+            print(f'{red}INDICE INVÁLIDO, TENTE NOVAMENTE!{clean}')
         else:
-            print('A MANIFESTAÇÃO DO ÍNDICE É: ' , ouvidoria[eliminar-1])
+            print(f'A MANIFESTAÇÃO DE ÍNDICE{eliminar} É {magenta}{ouvidoria[eliminar - 1]}{magenta}')
             confirma = (input("DESEJA CONTINUAR? [SIM/NÃO]: ")).lower()
             if confirma == "sim":
                 ouvidoria.pop(eliminar-1)
-                print("MANIFESTAÇÃO ELIMINADA COM SUCESSO!")
+                print(f"{green}MANIFESTAÇÃO ELIMINADA COM SUCESSO!{clean}")
             else:
                 print("OPERAÇÃO CANCELADA COM SUCESSO!!!")
 
@@ -70,7 +85,7 @@ while True:
         print('AGRADECEMOS A PREFERÊNCIA')
         break
     else: 
-        print("RESPOSTA INVALIDA, TENTE NOVAMENTE!")
+        print(f'{red}VALOR INVÁLIDO, INSIRA APENAS OS VALORES LISTADOS NO MENU{clean}')
 
 
 
