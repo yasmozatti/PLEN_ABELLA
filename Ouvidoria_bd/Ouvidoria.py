e1fce601fc4ffa51047bd31546c5774cc62e249e
Kagimon <karlosralbuquerque@gmail.com>
2025-06-10 16:29:02 -0300

    Implementação de função de remover por código

--- Ouvidoria_bd/Ouvidoria.py
+++ Ouvidoria_bd/Ouvidoria.py
@@ -12,7 +12,7 @@ conn = db.criarConexao("localhost", "root", "", "ouvidoria")
 def criarTabelaOuvidoria():
     sql = """
     CREATE TABLE IF NOT EXISTS ouvidoria (
-        id INT AUTO_INCREMENT PRIMARY KEY,
+        codigo INT AUTO_INCREMENT PRIMARY KEY,
         nome VARCHAR(100),
         categoria ENUM('reclamação', 'sugestão', 'elogio'),
         manifestação TEXT
@@ -83,39 +83,110 @@ def menu():
 
 #Função para registrar uma ouvidoria
 def adicionar_manifestacao():
+    
     nome = input("Digite seu nome: ")
-    categoria = input("Categoria (reclamação/sugestão/elogio): ").lower()
-    mensagem = input("Digite sua manifestação: ")
+    
+    while True:
+
+        categoria = input(f"Categorias
+                        \n{green}[1]{clean}reclamação
+                        \n{red}[2]{clean}sugestão
+                        \n{cyan}elogio:{clean} ")
+    
+    
+        if categoria == "1":
+            categoria = "reclamação"
+            break
+
+        elif categoria == "2":
+            categoria = "sugestão"
+            break
+
+        elif categoria == "3":
+            categoria = "elogio"
+            break
+            
+        else:
+            print(f"{red}Categoria inválida. Por favor, escolha uma opção válida.{clean}")
+        
+    manifestacao = input("Digite sua manifestação: ")
 
     sql = "INSERT INTO ouvidoria (nome, categoria, manifestação) VALUES (%s, %s, %s)"
-    dados = (nome, categoria, mensagem)
+    dados = (nome, categoria, manifestacao)
 
     id = db.insertNoBancoDados(conn, sql, dados)
     if id:
         print("Manifestação registrada com sucesso.")
 
+
 # Função para listar as mensagens da ouvidoria
 def listar_todas_manifestacoes():
+    
     sql = "SELECT id, nome, categoria, manifestação FROM ouvidoria"
     resultados = db.listarBancoDados(conn, sql)
     for ouvidoria in resultados:
-        print(f"ID: {ouvidoria[0]}, \nNOME: {ouvidoria[1]} \nCATEGORIA: {ouvidoria[2]}, \nMANIFESTAÇÃO: {ouvidoria[3]}")
+        print(f"CODIGO: {ouvidoria[0]}, \nNOME: {ouvidoria[1]} \nCATEGORIA: {ouvidoria[2]}, \nMANIFESTAÇÃO: {ouvidoria[3]}")
 
 #Função para listar as mensagens por categoria
 def listar_manifestacoes_tipo():
-    categoria_ouvidoria = input("Informa a categoria")
-    sql = f"SELECT id, nome, categoria, manifestação FROM ouvidoria WHERE categoria = '{categoria_ouvidoria}'"
+    while True:
+
+        categoria_manifestacao = input(f"Informe a categoria que deseja listar:
+                        \n{green}[1]{clean}reclamação
+                        \n{red}[2]{clean}sugestão
+                        \n{cyan}elogio:{clean} ")
+    
+    
+        if categoria_manifestacao == "1":
+            categoria_manifestacao = "reclamação"
+            break
+
+        elif categoria_manifestacao == "2":
+            categoria_manifestacao = "sugestão"
+            break
+
+        elif categoria_manifestacao == "3":
+            categoria_manifestacao = "elogio"
+            break
+        
+        else:
+            print(f"{red}Categoria inválida. Por favor, escolha uma opção válida.{clean}")
+
+    sql = f"SELECT codigo, nome, categoria, manifestação FROM ouvidoria WHERE categoria = '{categoria_manifestacao}'"
     resultados = db.listarBancoDados(conn, sql)
     for ouvidoria in resultados:
         print(f"ID: {ouvidoria[0]}, \nNOME: {ouvidoria[1]} \nCATEGORIA: {ouvidoria[2]}, \nMANIFESTAÇÃO: {ouvidoria[3]}")
 
 #Função para excluir alguma ouvidoria
 def remover_manifestacao():
-    print("Implementar")
+    
+    while True:
+    
+        try:
+            codigo_de_remoção = int(input("Digite o código da manifestação a ser removida: "))
+            break
+            
+        except ValueError:
+            print(f"{red}Por favor, insira um número inteiro para o código.{clean}")
+
+
+    sql = "DELETE FROM ouvidoria WHERE codigo = %s"
+    dados = (codigo_de_remoção)
+    resultado = db.
+    manifestacao_excluida = excluirBancoDados(connection, sql, dados)
+    
+    if manifestacao_excluida > 0:
+        print(f"{green}Manifestação com código {codigo_de_remoção} removida com sucesso!{clean}")
+
+    else:
+        print(f"{red}Nenhuma manifestação encontrada com o código {codigo_de_remoção}.{clean}")
 
 #Função para procurar uma manifestacao por códigona 
 def procurar_manifestacao():
-    print("Implementar")
+    codigo_manifestacao = int(input("Digite o código da manifestação a ser pesquisada: "))
+    consulta = 'select * from Ouvidoria where codigo = %s'
+    dados = [ codigo_manifestacao ]
+    filmes = listarBancoDados(conexao,consulta,dados)
 
 #Executando o programa
 menu()
